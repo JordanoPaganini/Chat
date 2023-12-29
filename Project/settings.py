b53eb48a5a1fcba246afe7354555113eb0f44fd2
Jor137005 <128003065+JordanoPaganini@users.noreply.github.com>
2023-12-29 18:20:05 -0300

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -125,7 +125,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("<URL>")],
+            "hosts": [("<URL_REDIS>")],
         },
     },
 }
